95569909f000b8bfa141161562c002ee4fa806a5
Anthony Liatsis <anthony@relateiq.com>
2015-11-13 13:35:20 -0800

    maybe create model value on render if it is not one

--- src/PillerCtrl.js
+++ src/PillerCtrl.js
@@ -3,71 +3,78 @@ var moduleName = 'PillerCtrl';
 module.exports = moduleName;
 
 angular.module(moduleName, [
-  require('./PillerSrvc')
+    require('./PillerSrvc')
 ])
 
 .controller(moduleName, function($scope, PillerSrvc) {
-  var PillerCtrl = this;
+    var PillerCtrl = this;
 
-  PillerCtrl.init = init;
-  PillerCtrl.initTextarea = initTextarea;
+    PillerCtrl.init = init;
+    PillerCtrl.initTextarea = initTextarea;
 
 
-  function init(container, ngModel) {
-    PillerCtrl.ngModel = PillerCtrl.ngModel || ngModel;
+    function init(container, ngModel) {
+        PillerCtrl.ngModel = PillerCtrl.ngModel || ngModel;
 
-    if (!PillerCtrl.ngModel) {
-      throw new Error('angular-piller: ng-model is required from <piller/> or [piller-textarea]');
+        if (!PillerCtrl.ngModel) {
+            throw new Error('angular-piller: ng-model is required from <piller/> or [piller-textarea]');
+        }
+
+        initOptions();
+
+        PillerCtrl.pillerInstance = PillerSrvc.create(container, function() {
+            return $scope.pillCorpus || [];
+        }, $scope.pillerOptions, PillerCtrl.textarea);
+
+        PillerCtrl.ngModel.$render = render;
+        PillerCtrl.ngModel.$parsers.push(parser);
+
+        watchOptions();
     }
 
-    initOptions();
+    function initTextarea(textarea, ngModel) {
+        PillerCtrl.ngModel = ngModel;
+        PillerCtrl.textarea = textarea;
+    }
 
-    PillerCtrl.pillerInstance = PillerSrvc.create(container, function() {
-      return $scope.pillCorpus || [];
-    }, $scope.pillerOptions, PillerCtrl.textarea);
+    function initOptions() {
+        $scope.pillerOptions = $scope.pillerOptions || {};
+        $scope.pillerOptions.onModelChange = onPillerModelChange;
+        $scope.pillerOptions.showSearchMatches = function(matches) {
+            $scope.showSearchMatches({
+                pillerInstance: PillerCtrl.pillerInstance,
+                matches: matches
+            });
+        };
+    }
 
-    PillerCtrl.ngModel.$render = render;
-    PillerCtrl.ngModel.$parsers.push(parser);
+    function watchOptions() {
+        $scope.$watchCollection('pillerOptions', function(newValue, oldValue) {
+            if (
+                (newValue && !oldValue) ||
+                (newValue && oldValue && newValue.storageKey !== oldValue.storageKey)
+            ) {
+                PillerCtrl.pillerInstance.reset();
+            }
+        });
+    }
 
-    watchOptions();
-  }
+    function render() {
+        if (PillerCtrl.ngModel.$modelValue && !PillerCtrl.ngModel.$modelValue._isPillerModelValue) {
+            PillerCtrl.ngModel.$modelValue = PillerCtrl.pillerInstance.createModelValue(
+                PillerCtrl.ngModel.$modelValue.text,
+                PillerCtrl.ngModel.$modelValue.pills
+            );
+        }
 
-  function initTextarea(textarea, ngModel) {
-    PillerCtrl.ngModel = ngModel;
-    PillerCtrl.textarea = textarea;
-  }
+        PillerCtrl.pillerInstance.modelValue = PillerCtrl.ngModel.$modelValue;
+    }
 
-  function initOptions() {
-    $scope.pillerOptions = $scope.pillerOptions || {};
-    $scope.pillerOptions.onModelChange = onPillerModelChange;
-    $scope.pillerOptions.showSearchMatches = function(matches) {
-      $scope.showSearchMatches({
-        pillerInstance: PillerCtrl.pillerInstance,
-        matches: matches
-      });
-    };
-  }
+    function parser() {
+        return PillerCtrl.pillerInstance.modelValue;
+    }
 
-  function watchOptions() {
-    $scope.$watchCollection('pillerOptions', function(newValue, oldValue) {
-      if (
-        (newValue && !oldValue) ||
-        (newValue && oldValue && newValue.storageKey !== oldValue.storageKey)
-      ) {
-        PillerCtrl.pillerInstance.reset();
-      }
-    });
-  }
-
-  function render() {
-    PillerCtrl.pillerInstance.modelValue = PillerCtrl.ngModel.$modelValue;
-  }
-
-  function parser() {
-    return PillerCtrl.pillerInstance.modelValue;
-  }
-
-  function onPillerModelChange(modelValue) {
-    PillerCtrl.ngModel.$setViewValue(modelValue && modelValue.text || '');
-  }
+    function onPillerModelChange(modelValue) {
+        PillerCtrl.ngModel.$setViewValue(modelValue && modelValue.text || '');
+    }
 });
